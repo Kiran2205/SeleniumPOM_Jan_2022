89cd0db65fc36971b65c3f3e7f414cb0f1d268ca
Kiran <kirank.m31990@gmail.com>
2021-12-29 14:47:48 +0100

    Made changes to Constructor of CommonsPage.java

--- src/main/java/com/qa/opencart/pages/CommonsPage.java
+++ src/main/java/com/qa/opencart/pages/CommonsPage.java
@@ -1,10 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class CommonsPage {
     //Search
     //Add to Cart
+    private WebDriver driver;
 
-    public CommonsPage() {
-
+    public CommonsPage(WebDriver driver) {
+        this.driver = driver;
     }
 }
