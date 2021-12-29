e6ad251e622402ea61e384657de667fe2ed4f5eb
Kiran <kirank.m31990@gmail.com>
2021-12-29 13:52:37 +0100

    Made changes to CommonsPage and added a new page Demo

--- src/main/java/com/qa/opencart/pages/CommonsPage.java
+++ src/main/java/com/qa/opencart/pages/CommonsPage.java
@@ -3,4 +3,8 @@ package com.qa.opencart.pages;
 public class CommonsPage {
     //Search
     //Add to Cart
+
+    public void CommonsPage() {
+
+    }
 }
---
+++ src/main/java/com/qa/opencart/pages/Demo.java
@@ -0,0 +1,8 @@
+package com.qa.opencart.pages;
+
+public class Demo {
+
+    public void printDemo() {
+        System.out.println("This is a demo method added in a demo class to learn git");
+    }
+}
